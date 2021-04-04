c8858fd832b3bb15df46658ae1bb3b97b5d3d668
nSalameh <74309258+NaderSalameh@users.noreply.github.com>
2021-04-03 20:13:58 -0400

    commiting final code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,21 +1,139 @@
-//table 
-var tbody = d3.select("tbody");
+// creating the dropdown list
+var drop_date = d3.select("#DropDown-Date");
+var drop_city = d3.select("#DropDown-City");
+var drop_state = d3.select("#DropDown-State");
+var drop_country = d3.select("#DropDown-Country");
+var drop_shape = d3.select("#DropDown-Shape");
 
 
+var date_list    = []
+var city_list    = []
+var state_list   = []
+var country_list = []
+var shape_list   = []
 
-// inspect the data
-console.log(data)
-
-
-data.forEach(item => 
+data.forEach(item =>
     {
+        if (!date_list.includes(item.datetime)) 
+        {
+            date_list.push(item.datetime);
+            drop_date.append("option").text(item.datetime)
+        }
 
-        var row = tbody.append("tr")
-        row.append("td").text(item.datetime);
-        row.append("td").text(item.city);
-        row.append("td").text(item.state);
-        row.append("td").text(item.country);
-        row.append("td").text(item.shape);
-        row.append("td").text(item.durationMinutes);
+        if (!city_list.includes(item.city)) 
+        {
+            city_list.push(item.city);
+            drop_city.append("option").text(item.city)
+        }
 
-    })
+        if (!state_list.includes(item.state)) 
+        {
+            state_list.push(item.state);
+            drop_state.append("option").text(item.state)
+        }
+
+        if (!country_list.includes(item.country)) 
+        {
+            country_list.push(item.country);
+            drop_country.append("option").text(item.country)
+        }
+
+        if (!shape_list.includes(item.shape)) 
+        {
+            shape_list.push(item.shape);
+            drop_shape.append("option").text(item.shape)
+        }
+
+    }    
+)
+
+//****** button drop down events *******//
+//--. creating the filer function to be passed through the event
+d3.select("#query-button").on("click", function() 
+    {
+        var filtered_data = data
+
+        inputDate    = d3.select("#DropDown-Date").property("value") 
+        inputCity    = d3.select("#DropDown-City").property("value") 
+        inputState   = d3.select("#DropDown-State").property("value") 
+        inputCountry = d3.select("#DropDown-Country").property("value") 
+        inputShape   = d3.select("#DropDown-Shape").property("value") 
+        
+
+
+        if (inputDate === "no filter") 
+        { 
+            filterData = filtered_data
+        }
+        else 
+        {
+            filterData = filtered_data.filter(item => item.datetime === inputDate)
+        }
+
+        if (inputCity === "no filter")
+        { 
+            filterData = filterData  
+        }
+        else
+        {
+            filterData = filterData.filter(item => item.city === inputCity)
+        }
+
+        if (inputState === "no filter")
+        { 
+            filterData = filterData 
+        }
+        else
+        {
+            filterData = filterData.filter(item => item.state === inputState) 
+        }
+
+        if (inputCountry === "no filter")
+        { 
+            filterData = filterData 
+        }
+        else
+        {
+            filterData = filterData.filter(item => item.country === inputCountry)
+        }
+
+        if (inputShape === "no filter")
+        {  
+            filterData = filterData 
+        }
+        else
+        {
+            filterData = filterData.filter(item => item.shape === inputShape) 
+        }
+
+ 
+        //table 
+        var tbody = d3.select("tbody");
+        tbody.html("")        
+        filterData.forEach(item => 
+             {
+                var row = tbody.append("tr")
+                row.append("td").text(item.datetime);
+                row.append("td").text(item.city);
+                row.append("td").text(item.state);
+                row.append("td").text(item.country);
+                row.append("td").text(item.shape);
+                row.append("td").text(item.durationMinutes);
+                row.append("td").text(item.comments.replace(/&#39/g, "'").replace(/&#44/g, ",").replace(/&#33/g, "!"));
+             }
+        )     
+    }    
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
